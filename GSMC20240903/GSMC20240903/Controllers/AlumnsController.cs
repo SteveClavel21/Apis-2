246d8bb27ec8103237b9f80bb049c24b99579a8c
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-09-03 20:11:49 -0600

    terminado

---
+++ GSMC20240903/GSMC20240903/Controllers/AlumnsController.cs
@@ -0,0 +1,70 @@
+﻿using GSMC20240903.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GSMC20240903.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnsController : ControllerBase
+    {
+        static List<Alumnos> alumnos = new List<Alumnos>();
+
+        // GET: api/<AlumnsController>
+        [HttpGet]
+        public IEnumerable<Alumnos> Get()
+        {
+            return alumnos;
+        }
+
+        // GET api/<AlumnsController>/5
+        [HttpGet("{id}")]
+        public Alumnos Get(int id)
+        {
+            var alumno = alumnos.FirstOrDefault(c => c.Id == id);
+            return alumno;
+        }
+
+        // POST api/<AlumnsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Alumnos alumno)
+        {
+            alumnos.Add(alumno);
+            return Ok();
+        }
+
+        // PUT api/<AlumnsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Alumnos alumno)
+        {
+            var existingAlumno = alumnos.FirstOrDefault(c => c.Id == id);
+            if (existingAlumno != null)
+            {
+                existingAlumno.Nombre = alumno.Nombre;
+                existingAlumno.Apellido = alumno.Apellido;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE api/<AlumnsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingAlumno = alumnos.FirstOrDefault(c => c.Id == id);
+            if (existingAlumno != null)
+            {
+                alumnos.Remove(existingAlumno);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ GSMC20240903/GSMC20240903/Models/Alumnos.cs
@@ -0,0 +1,9 @@
+﻿namespace GSMC20240903.Models
+{
+    public class Alumnos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido {  get; set; }
+    }
+}
